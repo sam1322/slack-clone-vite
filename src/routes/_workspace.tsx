223de92be100d0c552619af767898a0ddae55a73
saminem <sriram.pant5@gmail.com>
2024-11-24 14:02:21 +0530

    feat(workspace): add resizable component from shadcn in workspace

--- src/routes/_workspace.tsx
+++ src/routes/_workspace.tsx
@@ -1,4 +1,9 @@
 import Toolbar from "@/components/Toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Outlet } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -10,7 +15,47 @@ function RouteComponent() {
   return (
     <div className="h-full">
       <Toolbar />
-      <Outlet />
+      <div className="flex h-[calc(100vh-40px)]">
+        {/* <Sidebar /> */}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            minSize={11}
+            defaultSize={20}
+            className="bg-[#5E2C5F]"
+          >
+            {/* <WorkspaceSidebar /> */}
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20} defaultSize={80}>
+            <Outlet />
+          </ResizablePanel>
+          {/* {showPanel && (
+            <>
+              <ResizableHandle withHandle />
+              <ResizablePanel minSize={20} defaultSize={29}>
+                {parentMessageId ? (
+                  <Thread
+                    messageId={parentMessageId as Id<"messages">}
+                    onClose={onClose}
+                  />
+                ) : profileMemberId ? (
+                  <Profile
+                    memberId={profileMemberId as Id<"members">}
+                    onClose={onClose}
+                  />
+                ) : (
+                  <div className="flex h-full items-center justify-center">
+                    <Loader className="size-5 animate-spin text-muted-foreground" />
+                  </div>
+                )}
+              </ResizablePanel>
+            </>
+          )} */}
+        </ResizablePanelGroup>
+      </div>
     </div>
   );
 }
