9df5ef7c0047fc75c19beaffcfacc3f473558255
saminem <sriram.pant5@gmail.com>
2024-11-27 01:04:41 +0530

    feat(sidebar): implement other buttons present in sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
+import { useLocation } from "@tanstack/react-router";
+import { SidebarButton } from "./SidebarButton";
 import WorkspaceSwitcher from "./WorkspaceSwitcher";
+import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react";
+import { UserButton } from "../auth/UserButton";
 
 const Sidebar = () => {
+  const { pathname } = useLocation();
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <WorkspaceSwitcher />
-      {/* <SidebarButton
+      <SidebarButton
         icon={Home}
         label="Home"
         isActive={pathname.includes("/workspace")}
@@ -14,7 +19,7 @@ const Sidebar = () => {
       <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
-      </div> */}
+      </div>
     </aside>
   );
 };
